0b7f6f24cc64c2ad81aa65c5d04331c340469509
Jesiah Loosman <Jesiah.Loosman@outlook.de>
2024-07-09 16:04:49 +0200

    basic login site added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container">
+  <h1>Find new Music</h1>
+  <p>A simple application for finding new music</p>
+  <a href="api/login">
+    <button>Login</button>
+  </a>
+</div>
+
+<style>
+  .container {
+    height: 100vh;
+    width: 100%;
+    padding-top: 200px;
+    background: var(--background);
+  }
+
+  .container > * {
+    margin: 15px;
+  }
+
+  h1 {
+    color: var(--primary);
+  }
+  p {
+    color: var(--secondary);
+  }
+  button {
+    padding: 10px 20px;
+    color: var(--secondary);
+    background-color: var(--primary);
+    border: none;
+    border-radius: 10px;
+  }
+</style>
