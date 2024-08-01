cbd787e099a2141a2af7222cedee78a0b6dd1b36
Jesiah Loosman <Jesiah.Loosman@outlook.de>
2024-08-01 23:40:08 +0200

    first base regarding foryou page

--- src/routes/explore/foryou/+page.svelte
+++ src/routes/explore/foryou/+page.svelte
@@ -1,5 +1,42 @@
 <script>
   import Slide from "$lib/components/Slide.svelte";
+  import { onMount } from "svelte";
+
+  let tracks = [];
+
+  $: console.log(tracks);
+
+  const getTracks = async (offset) => {
+    const tracks = await fetch(`/api/recommendations/songs?offset=${offset}`);
+
+    return tracks.json();
+  };
+
+  onMount(() => {
+    getTracks(0).then((value) => {
+      tracks = [...tracks, ...value.tracks];
+    });
+  });
 </script>
 
-<Slide />
+{#each tracks as track, i}
+  <div class="item">
+    <Slide
+      index={i}
+      imageUrl={track.album.images[0].url}
+      name={track.name}
+      artist={track.artists[0].name}
+      preview={track.preview_url}
+    />
+  </div>
+{/each}
+
+<style>
+  .item {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+
+    margin-top: 200px;
+  }
+</style>
